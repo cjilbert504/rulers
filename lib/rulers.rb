bc57bbf8741e782547f8d87a04f027f4c4dc4f47
Collin Jilbert <cjilbert504@gmail.com>
2021-08-24 08:09:17 -0500

    Setup public folder with an index.html page, currently not able to read the file though

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,9 +6,21 @@ module Rulers
   class Application
     def call(env)
       # `echo debug > debug.txt`;
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {"Content-Type" => "text/html"}, []]
+      end
+      
+      # if env["PATH_INFO"] == "/"
+      #   return [200, {"Content-Type" => "text/html"}, [File.read("public/index.html")]] #Not sure why this isn't able to find this file.
+      # end
+      
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
+      begin
+        text = controller.send(act)
+      rescue RuntimeError
+        text = "<h1><em>Sorry, we don't know what to do with that</em></h1>"
+      end
       [200, {"Content-Type" => "text/html"}, [text]]
     end
   end
